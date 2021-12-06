5e4ef7c3f36ce03d9f50ff94fca3928fad9ae9d1
Sarah-Alqahtani <91519771+Sarah-Alqahtani@users.noreply.github.com>
2021-12-06 18:26:22 +0000

    changing

--- Coldcall/Coldcall/ViewController.swift
+++ Coldcall/Coldcall/ViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var Numincrease: UILabel!
+    var increase=0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     @IBAction func btn(_ sender: Any) {
-    
-    let Rname:[String]=["RINAI","Joli","Sarah","Nora","joo","Qahtan"]
+        
+        
+        let Rname:[String]=["RINAI","Joli","Sarah","Nora","joo","Qahtan"]
+        
         NameLabel.text=Rname.randomElement()
+        increase+=1
+        Numincrease.text=String(increase)
+       
+        for _ in 1...7{
+           
+            if increase<7{
+            switch (increase)  {
+            case 1:
+                Numincrease.textColor = UIColor.red
+                // statements
 
+              case 2:
+                Numincrease.textColor = UIColor.red
+       
+            case 3:
+                Numincrease.textColor = UIColor.orange
+            case 4:
+                Numincrease.textColor = UIColor.orange
+            case 5:
+                Numincrease.textColor = UIColor.green
+            
+                    
+              default:
+                Numincrease.textColor = UIColor.black
+                break
+            }
+       
 
-    }
+            }
+            }
+            
+        }}
+  
+        
+  
+ 
     
-}
+
+    
+
